55408ab2d9241ddc4c55561dec43baf5ca72861e
Saurabh1997 <patilsaurabh.2013@gmail.com>
2024-09-26 20:12:55 +0530

    changes in dev and start script

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema/schema.js";
-import { activities, users } from "./db/_db.js";
+import { activities, activity_documents, users } from "./db/_db.js";
 
 const resolvers = {
   Query: {
@@ -17,12 +17,35 @@ const resolvers = {
     activity(_: any, args: { id: string }) {
       return activities.find((activity) => activity.id === args.id);
     },
+    activity_documents() {
+      return activity_documents;
+    },
+    activity_document(_: any, args: { id: string }) {
+      return activity_documents.find(
+        (activity_document) => activity_document.id === args.id
+      );
+    },
   },
   User: {
     activities(parent: { id: string }) {
       return activities.filter((activity) => activity.user_id === parent.id);
     },
   },
+  Activity: {
+    users(parent: { user_id: string }) {
+      return users.filter((user) => user.id === parent.user_id);
+    },
+    activity_document(parent: { id: string }) {
+      return activity_documents.find(
+        (document) => document.activity_id === parent.id
+      );
+    },
+  },
+  // Activity_document: {
+  //   users(parent: { user_id: string }) {
+  //     return users.filter((user) => user.id === parent.user_id);
+  //   },
+  // },
 };
 
 // two properties required - typeDefs , resolvers
